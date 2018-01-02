5654c7e40f807f0f1c487e1a68f2ef5c441dcc0c
SnowDWolf <snowdwolfe@gmail.com>
2018-01-01 23:59:41 -0600

    v12

---
+++ v10/app.js
@@ -0,0 +1,50 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    methodOverride = require('method-override'),
+    Campground = require("./models/campground"),
+    seedDB = require("./seeds"),
+    Comment = require("./models/comment"),
+    passport = require("passport"),
+    LocalStrategy = require('passport-local'),
+    User = require('./models/user');
+
+// Requiring Routes
+var commentRoutes = require('./routes/comments'),
+    campgroundRoutes = require('./routes/campgrounds'),
+    indexRoutes = require('./routes/index');
+
+// seedDB(); /* Seed The DataBase */
+mongoose.connect("mongodb://localhost/yelp_camp", { useMongoClient: true });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({
+    secret: "Let Winter Blanket The World In Snow",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+})
+
+app.use("/", indexRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/campgrounds", campgroundRoutes);
+
+
+
+app.listen(5000, function() {
+    console.log("YelpCamp started");
+});
